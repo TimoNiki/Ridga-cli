f26169a3fbf8ee834c052b54d918545fceab19bb
TimoNiki <295574811+TimoNiki@users.noreply.github.com>
2026-08-06 18:59:18 +0300

    Update installer.

--- installer.sh
+++ installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "🚀 Запуск установщика Ridga для macOS..."
+echo "🚀 Starting ridga installer"
 
 if ! command -v brew &> /dev/null; then
-    echo "⏳ Homebrew не найден. Устанавливаем..."
-    /bin/bash -c "$(curl -fsSL https://githubusercontent.com)"
+    echo "⏳ No homebrew on your mac. The program works without Homebrew, but applications listed in the Homebrew section cannot be installed."
+    echo "if errors without homebrew send letter to timonikivopros.team@gmail.com"
 else
-    echo "✅ Homebrew уже установлен."
+    echo "✅ Homebrew installed on mac. And if it isn't installed, programs from the HOMEBREW list won't be able to install."
 fi
 
 SRC_URL="https://raw.githubusercontent.com/TimoNiki/Ridga-cli/refs/heads/main/Ridga"
@@ -15,14 +15,11 @@ VERIFY_SRC_URL="https://raw.githubusercontent.com/TimoNiki/Ridga-cli/refs/heads/
 DEST_PATH="/usr/local/bin/Ridga"
 VERIFY_DEST_PATH="/usr/local/bin/ridga_verify"
 
-echo "⏳ Установка компонентов..."
+echo "⏳ Component Installing..."
 sudo curl -fsSL "$SRC_URL" -o "$DEST_PATH"
 sudo chmod +x "$DEST_PATH"
 
 sudo curl -fsSL "$VERIFY_SRC_URL" -o "$VERIFY_DEST_PATH"
 sudo chmod +x "$VERIFY_DEST_PATH"
 
-echo "🎉 Готово! Компоненты установлены."
-echo "Используйте команды:"
-echo "  Ridga install имя_программы"
-echo "  Ridga verify ПУТЬ_К_ПРОГРАММE.app"
+echo "🎉 Ready! ridga installed on your mac."
